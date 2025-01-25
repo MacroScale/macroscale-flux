643c9241306e8a867c2d4d1184156b53b4ff49c5
pgxtips <brandongill123@gmail.com>
2025-01-25 15:54:50 +0000

    feat: changed to current thread rt, implemented hotkeys

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,6 +1,15 @@
-use std::time::Duration;
+use std::{ptr, time::Duration};
 
-use tokio::{sync::{mpsc, watch}, time};
+use tokio::sync::{mpsc, watch};
+use tokio::task::spawn_local;
+use tokio::time;
+
+use windows::{
+    core::Result,
+    Win32::Foundation::*,
+    Win32::UI::Input::KeyboardAndMouse::*,
+    Win32::UI::WindowsAndMessaging::*,
+};
 
 pub enum Action {
     Quit,
@@ -8,12 +17,52 @@ pub enum Action {
     UploadCapture,
 }
 
-pub fn get_action(input: char) -> Option<Action>{
-    match input {
-        'q' => Some(Action::Quit),
-        'w' => Some(Action::CaptureGameplay),
-        'e' => Some(Action::UploadCapture),
-        _ => None
+/// register_hotkeys
+/// will be running headless, meaning no window handle supplied
+/// This also means that the only way to recieve input is to poll
+/// the message queue and check if a hotkey was pressed
+pub fn register_hotkeys() -> Result<()> {
+    log::info!("Registering hotkeys");
+    unsafe {
+        let quit_hotkey = RegisterHotKey(
+            Some(HWND(ptr::null_mut())),
+            1,
+            MOD_ALT,
+            'Q' as u32,
+        );
+        log::info!("Registered Quit Hotkey: ALT + SHIFT + Q")
+    };
+    Ok(())
+}
+
+pub async fn poll_hotkeys(shutdown_rx: watch::Receiver<bool>){
+    log::info!("starting polling hotkeys service");
+    let mut interval = time::interval(Duration::from_millis(50));
+
+    loop {
+        unsafe {
+            let mut msg = MSG {
+                hwnd: HWND(ptr::null_mut()),
+                message: 0,
+                wParam: WPARAM(0),
+                lParam: LPARAM(0),
+                time: 0,
+                pt: std::mem::zeroed(),
+            };
+
+            let peek_value = PeekMessageW(&mut msg, None, 0, 0, PM_REMOVE).as_bool();
+            if  peek_value {
+                if msg.message == WM_HOTKEY {
+                    log::info!("Win + s pressed!");
+                }
+                log::info!("Peek value {} Message Value: {}", peek_value, msg.message);
+            }
+        }
+        if *shutdown_rx.borrow() {
+            log::info!("Shutdown signal received: exiting hotkeys polling service");
+            break;
+        }
+        interval.tick().await;
     }
 }
 
@@ -22,28 +71,28 @@ pub async fn capture_action(tx: mpsc::Sender<Action>, shutdown_rx: watch::Receiv
     let mut interval = time::interval(Duration::from_millis(50));
 
     loop {
-
         interval.tick().await;
-
         if *shutdown_rx.borrow() {
             log::info!("shutdown signal received: exiting capture action service");
             break;
-        }
-
-        // get keyboard input from os interrupt
-        let input = 'q';
-
-        if let Some(action) = get_action(input) {
-            if let Err(err) = tx.send(action).await {
-                log::error!("Failed to send action: {}", err);
-            }
         }
     }
 }
 
 pub async fn execute_action(mut rx: mpsc::Receiver<Action>, shutdown_tx: watch::Sender<bool>){
-    log::info!("starting exectute action service");
-    while let Some(action) = rx.recv().await {
+    log::info!("starting execute action service");
+
+    loop {
+        let action_res = rx.recv().await;
+
+        let action = match action_res{
+            Some(a) => a,
+            None => {
+                log::error!("channel closed: exiting execute action service");
+                return;
+            } 
+        };
+
         match action {
             Action::Quit => { let _ = shutdown_tx.send(true); },
             Action::CaptureGameplay => log::info!("Capturing gameplay..."),
@@ -52,22 +101,28 @@ pub async fn execute_action(mut rx: mpsc::Receiver<Action>, shutdown_tx: watch::
 
         if *shutdown_tx.borrow() {
             log::info!("shutdown signal received: exiting execute action service");
-            break;
+            return;
         }
     }
 }
 
 pub async fn start(){
     log::info!("Starting Macroscale Game Capture");
+    let _ = register_hotkeys();
 
     let (tx, rx) = mpsc::channel(100);
     let (shutdown_tx, shutdown_rx) = watch::channel(false);
 
-    let capture_action_handle = tokio::spawn(capture_action(tx, shutdown_rx.clone()));
-    let execute_action_handle = tokio::spawn(execute_action(rx, shutdown_tx));
+    let poll_hotkeys_handle = spawn_local(poll_hotkeys(shutdown_rx.clone()));
+    let capture_action_handle = spawn_local(capture_action(tx, shutdown_rx.clone()));
+    let execute_action_handle = spawn_local(execute_action(rx, shutdown_tx));
 
     // Wait for all tasks to terminate after shutdown is triggered
-    let _ = tokio::join!(capture_action_handle, execute_action_handle);
+    let _ = tokio::join!(
+        poll_hotkeys_handle,
+        capture_action_handle,
+        execute_action_handle
+    );
 
     log::info!("Exiting Macroscale Game Capture");
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,27 @@
 use std::env;
 
+use tokio::task;
+
 //use win_x86_64;
 
 mod client;
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
     // init logging
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    client::start().await;
+    // Construct a local task set that can run `!Send` futures.
+    let local = task::LocalSet::new();
+
+    // Run the local task set.
+    local.run_until(async move {
+        // `spawn_local` ensures that the future is spawned on the local
+        // task set.
+        task::spawn_local(client::start()).await.unwrap();
+    }).await;
+
+    //client::start().await;
+    //client::test_polling();
 }
