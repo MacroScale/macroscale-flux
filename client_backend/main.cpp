f8c239524817704516cb3ee11fa6eb2199bf70d6
pgxtips <brandongill123@gmail.com>
2025-02-16 15:34:20 +0000

    feat: added gitignore

---
+++ client_backend/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "libs/NvFBC/inc/NvFBC.h"
+
+void initNvfbc(){
+    
+    std::cout << "initialised NvFBC successfully";
+}
+
+int main() {
+    initNvfbc();
+
+    std::cout << "exiting";
+    return 0;
+}
